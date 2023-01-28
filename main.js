d6f5599f234baa9dbd4f35973497b11ef005af49
Grzegorz Piejko <gp23.1982@gmail.com>
2023-01-28 09:08:29 +0100

    main.js (no additional functionality in calculator added, just code formatted and simplify): 1. remove clickedTableCells and clickedPreviousTableCellsSiblings arrays. 2. simplify tableCell.addEventListener 3.  replace one calculateAndDisplayResult function with two functions : calculateResult and presentResult 4. modify currencyInput, selectForm and selectTo addEventListeners. 5. create activateSelectFrom and activateSelectTo functions 6. modify selectFromEvent and selectToEvent. 7. reorganize the code.

--- main.js
+++ main.js
@@ -1,212 +1,78 @@
-const currencyInput = document.querySelector(".js-currency__input");
-const tableCells = document.querySelectorAll(".table__cell");
-const currencyRatioInput = document.querySelector(
-  ".js-form__field--currencyRatio"
-);
-const calculationButton = document.querySelector(".js-form__calculationButton");
-const selectFrom = document.querySelector(".js-form__field--currencyFrom");
-const selectTo = document.querySelector(".js-form__field--currencyTo");
-const result = document.querySelector(".js-form__result");
-const resetButton = document.querySelector(".js-form__reset");
-let clickedTableCells = [];
-let clickedPreviousTableCellsSiblings = [];
-const tableCellsArray = [...tableCells];
-const form = document.querySelector(".js-form");
+{
+  const currencyInput = document.querySelector(".js-currency__input");
+  const tableCells = document.querySelectorAll(".table__cell");
+  const currencyRatioInput = document.querySelector(
+    ".js-form__field--currencyRatio"
+  );
+  const calculationButton = document.querySelector(
+    ".js-form__calculationButton"
+  );
+  const selectFrom = document.querySelector(".js-form__field--currencyFrom");
+  const selectTo = document.querySelector(".js-form__field--currencyTo");
+  const result = document.querySelector(".js-form__result");
+  const resetButton = document.querySelector(".js-form__reset");
+  const tableCellsArray = [...tableCells];
+  const form = document.querySelector(".js-form");
 
-const resetFunction = () => {
-  currencyInput.classList.remove("active");
-  selectFrom.classList.remove("active");
-  selectTo.classList.remove("active");
-  currencyRatioInput.classList.remove("active");
-  tableCellsArray.forEach((tableCell) => tableCell.classList.remove("active"));
-  calculationButton.classList.add("form__calculationButton--disabled");
-  clickedTableCells = [];
-  clickedPreviousTableCellsSiblings = [];
-  result.innerText = "Wynik:";
-};
+  const resetFunction = () => {
+    currencyInput.classList.remove("active");
+    selectFrom.classList.remove("active");
+    selectTo.classList.remove("active");
+    currencyRatioInput.classList.remove("active");
+    tableCellsArray.forEach((tableCell) =>
+      tableCell.classList.remove("active")
+    );
+    calculationButton.classList.add("form__calculationButton--disabled");
+    result.innerText = "Wynik:";
+  };
 
-tableCellsArray.forEach((tableCell) => {
-  tableCell.addEventListener("click", (e) => {
-    if (isNaN(+tableCell.innerText)) return;
-    resetFunction();
-    if (currencyInput.value) {
-      calculationButton.classList.remove("form__calculationButton--disabled");
-    } else if ((currencyInput.value = "")) {
-      calculationButton.classList.add("form__calculationButton--disabled");
+  const currencyAutoSelection = (tableCell) => {
+    const threeFirstLetters = tableCell.previousElementSibling.innerText.slice(
+      0,
+      3
+    );
+    const threeLastLetters =
+      tableCell.previousElementSibling.innerText.slice(-3);
+    switch (threeFirstLetters) {
+      case "EUR":
+        selectFrom.value = "EUR";
+        break;
+      case "USD":
+        selectFrom.value = "USD";
+        break;
+      case "GBP":
+        selectFrom.value = "GBP";
+        break;
+      case "CHF":
+        selectFrom.value = "CHF";
+        break;
+      case "PLN":
+        selectFrom.value = "PLN";
+        break;
+      default:
+        break;
     }
-
-    const currencyAutoSelection = () => {
-      const threeFirstLetters =
-        tableCell.previousElementSibling.innerText.slice(0, 3);
-      const threeLastLetters =
-        tableCell.previousElementSibling.innerText.slice(-3);
-      switch (threeFirstLetters) {
-        case "EUR":
-          selectFrom.value = "EUR";
-          break;
-        case "USD":
-          selectFrom.value = "USD";
-          break;
-        case "GBP":
-          selectFrom.value = "GBP";
-          break;
-        case "CHF":
-          selectFrom.value = "CHF";
-          break;
-        case "PLN":
-          selectFrom.value = "PLN";
-          break;
-        default:
-          break;
-      }
-      switch (threeLastLetters) {
-        case "EUR":
-          selectTo.value = "EUR";
-          break;
-        case "USD":
-          selectTo.value = "USD";
-          break;
-        case "GBP":
-          selectTo.value = "GBP";
-          break;
-        case "CHF":
-          selectTo.value = "CHF";
-          break;
-        case "PLN":
-          selectTo.value = "PLN";
-          break;
-        default:
-          break;
-      }
-    };
-
-    if (+e.target.innerText > 0) {
-      currencyRatioInput.value = (+e.target.innerText).toFixed(4);
+    switch (threeLastLetters) {
+      case "EUR":
+        selectTo.value = "EUR";
+        break;
+      case "USD":
+        selectTo.value = "USD";
+        break;
+      case "GBP":
+        selectTo.value = "GBP";
+        break;
+      case "CHF":
+        selectTo.value = "CHF";
+        break;
+      case "PLN":
+        selectTo.value = "PLN";
+        break;
+      default:
+        break;
     }
+  };
 
-    if (
-      (+tableCell.innerText).toFixed(4) ===
-      (+currencyRatioInput.value).toFixed(4)
-    ) {
-      if (tableCell.classList.contains("active")) {
-        return;
-      }
-
-      if (
-        !tableCell.classList.contains("active") &&
-        !tableCell.previousElementSibling.classList.contains("active")
-      ) {
-        selectFrom.classList.add("active");
-        selectTo.classList.add("active");
-        currencyRatioInput.classList.add("active");
-
-        clickedTableCells.push(tableCell);
-        clickedPreviousTableCellsSiblings.push(
-          tableCell.previousElementSibling
-        );
-        tableCell.classList.add("active");
-        tableCell.previousElementSibling.classList.add("active");
-        if (
-          clickedTableCells.length === 2 &&
-          clickedPreviousTableCellsSiblings.length === 2
-        ) {
-          clickedTableCells[0].classList.remove("active");
-          clickedPreviousTableCellsSiblings[0].classList.remove("active");
-          clickedTableCells.shift();
-          clickedPreviousTableCellsSiblings.shift();
-        }
-      }
-
-      if (currencyInput.value) {
-        currencyInput.classList.add("active");
-      }
-    }
-
-    currencyAutoSelection();
-  });
-});
-
-const currencyInputEvent = () => {
-  if (currencyInput.value < 1) {
-    currencyInput.value = "";
-  }
-  currencyInput.value !== ""
-    ? currencyInput.classList.add("active")
-    : currencyInput.classList.remove("active");
-};
-
-const selectFromEvent = () => {
-  tableCellsArray.forEach((tableCell) => tableCell.classList.remove("active"));
-
-  clickedTableCells.length = 0;
-  clickedPreviousTableCellsSiblings.length = 0;
-
-  switch (selectFrom.value) {
-    case "PLN":
-    case "USD":
-    case "GBP":
-    case "EUR":
-    case "CHF":
-      selectFrom.classList.add("active");
-      break;
-    case "":
-      selectFrom.classList.remove("active");
-    default:
-      break;
-  }
-};
-
-const selectToEvent = () => {
-  tableCellsArray.forEach((tableCell) => tableCell.classList.remove("active"));
-
-  clickedTableCells.length = 0;
-  clickedPreviousTableCellsSiblings.length = 0;
-
-  switch (selectTo.value) {
-    case "PLN":
-    case "USD":
-    case "GBP":
-    case "EUR":
-    case "CHF":
-      selectTo.classList.add("active");
-      break;
-    case "":
-      selectTo.classList.remove("active");
-    default:
-      break;
-  }
-};
-
-const calculateAndDisplayResult = () => {
-  result.innerText = `Wynik: Za ${currencyInput.value} ${
-    selectFrom.value
-  } kupisz ${(currencyInput.value * currencyRatioInput.value).toFixed(2)} ${
-    selectTo.value
-  } `;
-};
-
-currencyInput.addEventListener("input", () => {
-  currencyInputEvent();
-});
-
-selectFrom.addEventListener("input", () => {
-  selectFromEvent();
-});
-
-selectTo.addEventListener("input", () => {
-  selectToEvent();
-});
-
-calculationButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  calculateAndDisplayResult();
-});
-
-resetButton.addEventListener("click", () => {
-  resetFunction();
-});
-
-form.addEventListener("input", () => {
   const activateTableCells = () => {
     if (
       selectFrom.value &&
@@ -214,11 +80,9 @@ form.addEventListener("input", () => {
       selectFrom.value !== selectTo.value
     ) {
       const combinedValues = `${selectFrom.value}/${selectTo.value}`;
-      console.log(combinedValues);
       const filteredCell = tableCellsArray.filter((tableCell) => {
         return tableCell.innerText === combinedValues;
       });
-      console.log(filteredCell[0].innerText);
       if (combinedValues === filteredCell[0].innerText) {
         filteredCell[0].classList.add("active");
         filteredCell[0].nextElementSibling.classList.add("active");
@@ -250,10 +114,111 @@ form.addEventListener("input", () => {
       !selectFrom.value ||
       !selectTo.value ||
       !currencyRatioInput.value
-    )
+    ) {
       calculationButton.classList.add("form__calculationButton--disabled");
+    }
   };
 
-  activateTableCells();
-  validateCalculationButton();
-});
+  const resetTableCellsArray = () => {
+    tableCellsArray.forEach((tableCell) =>
+      tableCell.classList.remove("active")
+    );
+  };
+
+  const currencyInputEvent = () => {
+    if (currencyInput.value < 1) {
+      currencyInput.value = "";
+    }
+    currencyInput.value !== ""
+      ? currencyInput.classList.add("active")
+      : currencyInput.classList.remove("active");
+  };
+
+  const activateSelectFrom = () => {
+    switch (selectFrom.value) {
+      case "PLN":
+      case "USD":
+      case "GBP":
+      case "EUR":
+      case "CHF":
+        selectFrom.classList.add("active");
+        break;
+      case "":
+        selectFrom.classList.remove("active");
+      default:
+        break;
+    }
+  };
+
+  const activateSelectTo = () => {
+    switch (selectTo.value) {
+      case "PLN":
+      case "USD":
+      case "GBP":
+      case "EUR":
+      case "CHF":
+        selectTo.classList.add("active");
+        break;
+      case "":
+        selectTo.classList.remove("active");
+      default:
+        break;
+    }
+  };
+
+  const selectFromEvent = () => {
+    resetTableCellsArray();
+    activateSelectFrom();
+  };
+
+  const selectToEvent = () => {
+    resetTableCellsArray();
+    activateSelectTo();
+  };
+
+  const calculateResult = () => {
+    return (currencyInput.value * currencyRatioInput.value).toFixed(2);
+  };
+
+  const presentResult = () => {
+    result.innerText = `Wynik: Za ${currencyInput.value} ${
+      selectFrom.value
+    } kupisz ${calculateResult()} ${selectTo.value} `;
+  };
+
+  tableCellsArray.forEach((tableCell) => {
+    tableCell.addEventListener("click", (e) => {
+      if (isNaN(+tableCell.innerText)) return;
+      currencyAutoSelection(tableCell);
+      currencyRatioInput.value = (+e.target.innerText).toFixed(4);
+      resetFunction();
+      validateCalculationButton();
+      selectFrom.classList.add("active");
+      selectTo.classList.add("active");
+      currencyRatioInput.classList.add("active");
+      tableCell.classList.add("active");
+      tableCell.previousElementSibling.classList.add("active");
+      if (currencyInput.value) {
+        currencyInput.classList.add("active");
+      }
+    });
+  });
+
+  currencyInput.addEventListener("input", currencyInputEvent);
+
+  selectFrom.addEventListener("input", selectFromEvent);
+
+  selectTo.addEventListener("input", selectToEvent);
+
+  calculationButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    presentResult();
+  });
+
+  resetButton.addEventListener("click", resetFunction);
+
+  form.addEventListener("input", () => {
+    activateTableCells();
+    validateCalculationButton();
+  });
+}
